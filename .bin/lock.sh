0a917633b05dfac3cd716b3b458b1c43923b9fe0
Fahad Hossain <oneloopone@gmail.com>
2016-08-25 04:26:47 +0600

    remove the crazy timer

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -6,29 +6,4 @@ convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte 
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
 
-# Wanted trigger timeout in milliseconds.
-IDLE_TIME=$((1*15*1000))
-
-# Sequence to execute when timeout triggers.
-trigger_cmd() {
-    xset dpms force off
-}
-
-sleep_time=$IDLE_TIME
-triggered=false
-
-# ceil() instead of floor()
-while sleep $(((sleep_time+999)/1000)); do
-    idle=$(xprintidle)
-    if [ $idle -ge $IDLE_TIME ]; then
-        if ! $triggered; then
-            trigger_cmd
-            triggered=true
-            sleep_time=$IDLE_TIME
-        fi
-    else
-        triggered=false
-        # Give 100 ms buffer to avoid frantic loops shortly before triggers.
-        sleep_time=$((IDLE_TIME-idle+100))
-    fi
-done
+sleep 15 && pgrep i3lock && xset dpms force off
