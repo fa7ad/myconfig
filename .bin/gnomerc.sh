c6d4565eb12951af73441a166e69e77ba61a0917
Fahad Hossain <oneloopone@gmail.com>
2016-09-25 01:16:36 +0600

    fix path problem with feh2gnome

--- .bin/gnomerc.sh
+++ .bin/gnomerc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 #nitrogen --restore --force-setter=gnome
-./feh2gnome
+feh2gnome
 gsettings set org.gnome.desktop.background show-desktop-icons true
 
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
