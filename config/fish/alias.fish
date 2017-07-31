f8b11581a6842172ede7c1ea965f0a6ede709e2e
Fahad Hossain <evilinfinite7@gmail.com>
2017-07-31 23:43:17 +0600

    alias -> abbr

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 
 # MPV
-alias mpv=mpvh
-alias mpa='mpv --no-vid'
+alias mpv mpvh
+alias mpa 'mpv --no-vid'
 
 # systemd
-alias systop='sudo systemctl stop'
-alias systart='sudo systemctl start'
-alias syrest='sudo systemctl restart'
+abbr systop 'sudo systemctl stop'
+abbr systart 'sudo systemctl start'
+abbr syrest 'sudo systemctl restart'
 
 # irssi
 alias irssi='screen irssi'
@@ -36,11 +36,10 @@ end
 
 # vim
 alias vim_clear_cache='rm -rf ~/.vim{backup,swap,undo,views}'
-alias vim_install_packages='vim +BundleInstall! +BundleClean! +q!'
 
 # Obvious short hands
 alias cls='clear'
-alias sshx='ssh -X'
+abbr sshx 'ssh -X'
 function mkcd
   mkdir -p $argv
   cd $argv
@@ -78,7 +77,6 @@ alias lc='lt -c'
 alias lk='ll -Sr'
 alias ll='ls -lh'
 alias lm='la | $PAGER'
-alias ln='nocorrect ln -i'
 alias lr='ll -tRFh'
 alias ls='ls --group-directories-first --color=auto'
 alias lsa='ls -lah'
@@ -94,177 +92,177 @@ alias lx='ll -XB'
 #
 
 # Git
-alias g='git'
-
 function git-current-branch
   echo (git branch|grep \* |sed s/\*\ //)
 end
 
-alias git='hub'
+alias git hub
 
-# Branch (b)
-alias gb='git branch'
-alias gba='git branch --all --verbose'
-alias gbc='git checkout -b'
-alias gbd='git branch --delete'
-alias gbD='git branch --delete --force'
-alias gbl='git branch --verbose'
-alias gbL='git branch --all --verbose'
-alias gbm='git branch --move'
-alias gbM='git branch --move --force'
-alias gbr='git branch --move'
-alias gbR='git branch --move --force'
-alias gbs='git show-branch'
-alias gbS='git show-branch --all'
-alias gbv='git branch --verbose'
-alias gbV='git branch --verbose --verbose'
-alias gbx='git branch --delete'
-alias gbX='git branch --delete --force'
+if not set -q git_aliases_set
+  set -U git_aliases_set
+  abbr g git
+  # Branch (b)
+  abbr gb 'git branch'
+  abbr gba 'git branch --all --verbose'
+  abbr gbc 'git checkout -b'
+  abbr gbd 'git branch --delete'
+  abbr gbD 'git branch --delete --force'
+  abbr gbl 'git branch --verbose'
+  abbr gbL 'git branch --all --verbose'
+  abbr gbm 'git branch --move'
+  abbr gbM 'git branch --move --force'
+  abbr gbr 'git branch --move'
+  abbr gbR 'git branch --move --force'
+  abbr gbs 'git show-branch'
+  abbr gbS 'git show-branch --all'
+  abbr gbv 'git branch --verbose'
+  abbr gbV 'git branch --verbose --verbose'
+  abbr gbx 'git branch --delete'
+  abbr gbX 'git branch --delete --force'
 
-# Commit (c)
-alias gc='git commit --verbose'
-alias gca='git commit --verbose --all'
-alias gcm='git commit --message'
-alias gcam='git commit --all --message'
-alias gco='git checkout'
-alias gcO='git checkout --patch'
-alias gcf='git commit --amend --reuse-message HEAD'
-alias gcF='git commit --verbose --amend'
-alias gcp='git cherry-pick --ff'
-alias gcP='git cherry-pick --no-commit'
-alias gcr='git revert'
-alias gcR='git reset "HEAD^"'
-alias gcs='git show'
-alias gcl='git-commit-lost'
+  # Commit (c)
+  abbr gc 'git commit --verbose'
+  abbr gca 'git commit --verbose --all'
+  abbr gcm 'git commit --message'
+  abbr gcam 'git commit --all --message'
+  abbr gco 'git checkout'
+  abbr gcO 'git checkout --patch'
+  abbr gcf 'git commit --amend --reuse-message HEAD'
+  abbr gcF 'git commit --verbose --amend'
+  abbr gcp 'git cherry-pick --ff'
+  abbr gcP 'git cherry-pick --no-commit'
+  abbr gcr 'git revert'
+  abbr gcR 'git reset "HEAD^"'
+  abbr gcs 'git show'
+  abbr gcl 'git-commit-lost'
 
-# Conflict (C)
-alias gCl='git --no-pager diff --name-only --diff-filter=U'
-alias gCa='git add (gCl)'
-alias gCe='git mergetool (gCl)'
-alias gCo='git checkout --ours --'
-alias gCO='gCo (gCl)'
-alias gCt='git checkout --theirs --'
-alias gCT='gCt (gCl)'
+  # Conflict (C)
+  abbr gCl 'git --no-pager diff --name-only --diff-filter=U'
+  abbr gCa 'git add (gCl)'
+  abbr gCe 'git mergetool (gCl)'
+  abbr gCo 'git checkout --ours --'
+  abbr gCO 'gCo (gCl)'
+  abbr gCt 'git checkout --theirs --'
+  abbr gCT 'gCt (gCl)'
 
-# Data (d)
-alias gd='git ls-files'
-alias gdc='git ls-files --cached'
-alias gdx='git ls-files --deleted'
-alias gdm='git ls-files --modified'
-alias gdu='git ls-files --other --exclude-standard'
-alias gdk='git ls-files --killed'
-alias gdi='git status --porcelain --short --ignored | sed -n "s/^!! //p"'
+  # Data (d)
+  abbr gd 'git ls-files'
+  abbr gdc 'git ls-files --cached'
+  abbr gdx 'git ls-files --deleted'
+  abbr gdm 'git ls-files --modified'
+  abbr gdu 'git ls-files --other --exclude-standard'
+  abbr gdk 'git ls-files --killed'
+  abbr gdi 'git status --porcelain --short --ignored | sed -n "s/^!! //p"'
 
-# Fetch (f)
-alias gf='git fetch'
-alias gfa='git fetch --all'
-alias gfc='git clone'
-alias gfm='git pull'
-alias gfr='git pull --rebase'
+  # Fetch (f)
+  abbr gf 'git fetch'
+  abbr gfa 'git fetch --all'
+  abbr gfc 'git clone'
+  abbr gfm 'git pull'
+  abbr gfr 'git pull --rebase'
 
-# Grep (g)
-alias gg='git grep'
-alias ggi='git grep --ignore-case'
-alias ggl='git grep --files-with-matches'
-alias ggL='git grep --files-without-matches'
-alias ggv='git grep --invert-match'
-alias ggw='git grep --word-regexp'
+  # Grep (g)
+  abbr gg 'git grep'
+  abbr ggi 'git grep --ignore-case'
+  abbr ggl 'git grep --files-with-matches'
+  abbr ggL 'git grep --files-without-matches'
+  abbr ggv 'git grep --invert-match'
+  abbr ggw 'git grep --word-regexp'
 
-# Index (i)
-alias gia='git add'
-alias giA='git add --patch'
-alias giu='git add --update'
-alias gid='git diff --no-ext-diff --cached'
-alias giD='git diff --no-ext-diff --cached --word-diff'
-alias gii='git update-index --assume-unchanged'
-alias giI='git update-index --no-assume-unchanged'
-alias gir='git reset'
-alias giR='git reset --patch'
-alias gix='git rm -r --cached'
-alias giX='git rm -rf --cached'
+  # Index (i)
+  abbr gia 'git add'
+  abbr giA 'git add --patch'
+  abbr giu 'git add --update'
+  abbr gid 'git diff --no-ext-diff --cached'
+  abbr giD 'git diff --no-ext-diff --cached --word-diff'
+  abbr gii 'git update-index --assume-unchanged'
+  abbr giI 'git update-index --no-assume-unchanged'
+  abbr gir 'git reset'
+  abbr giR 'git reset --patch'
+  abbr gix 'git rm -r --cached'
+  abbr giX 'git rm -rf --cached'
 
-# Log (l)
-alias gl='git log --topo-order --pretty'
+  # Log (l)
+  abbr gl 'git log --topo-order --pretty'
 
-# Merge (m)
-alias gm='git merge'
-alias gmC='git merge --no-commit'
-alias gmF='git merge --no-ff'
-alias gma='git merge --abort'
-alias gmt='git mergetool'
+  # Merge (m)
+  abbr gm 'git merge'
+  abbr gmC 'git merge --no-commit'
+  abbr gmF 'git merge --no-ff'
+  abbr gma 'git merge --abort'
+  abbr gmt 'git mergetool'
 
-# Push (p)
-alias gp='git push'
-alias gpf='git push --force-with-lease'
-alias gpF='git push --force'
-alias gpa='git push --all'
-alias gpA='git push --all; git push --tags'
-alias gpt='git push --tags'
-function gpc
-  git push --set-upstream origin (git-branch-current)
+  # Push (p)
+  abbr gp 'git push'
+  abbr gpf 'git push --force-with-lease'
+  abbr gpF 'git push --force'
+  abbr gpa 'git push --all'
+  abbr gpA 'git push --all; git push --tags'
+  abbr gpt 'git push --tags'
+  abbr gpc 'git push --set-upstream origin (git-branch-current)'
+
+  function gpp
+    git pull origin (git-branch-current)
+    git push origin (git-branch-current)
+  end
+
+  # Rebase (r)
+  abbr gr 'git rebase'
+  abbr gra 'git rebase --abort'
+  abbr grc 'git rebase --continue'
+  abbr gri 'git rebase --interactive'
+  abbr grs 'git rebase --skip'
+
+  # Remote (R)
+  abbr gR 'git remote'
+  abbr gRl 'git remote --verbose'
+  abbr gRa 'git remote add'
+  abbr gRx 'git remote rm'
+  abbr gRm 'git remote rename'
+  abbr gRu 'git remote update'
+  abbr gRp 'git remote prune'
+  abbr gRs 'git remote show'
+  abbr gRb 'git-hub-browse'
+
+  # Stash (s)
+  abbr gs 'git stash'
+  abbr gsa 'git stash apply'
+  abbr gsx 'git stash drop'
+  abbr gsX 'git-stash-clear-interactive'
+  abbr gsl 'git stash list'
+  abbr gsL 'git-stash-dropped'
+  abbr gsd 'git stash show --patch --stat'
+  abbr gsp 'git stash pop'
+  abbr gsr 'git-stash-recover'
+  abbr gss 'git stash save --include-untracked'
+  abbr gsS 'git stash save --patch --no-keep-index'
+  abbr gsw 'git stash save --include-untracked --keep-index'
+
+  # Submodule (S)
+  abbr gS 'git submodule'
+  abbr gSa 'git submodule add'
+  abbr gSf 'git submodule foreach'
+  abbr gSi 'git submodule init'
+  abbr gSI 'git submodule update --init --recursive'
+  abbr gSl 'git submodule status'
+  abbr gSm 'git-submodule-move'
+  abbr gSs 'git submodule sync'
+  abbr gSu 'git submodule foreach git pull origin master'
+  abbr gSx 'git-submodule-remove'
+
+  # Working Copy (w)
+  abbr gwd 'git diff --no-ext-diff'
+  abbr gwD 'git diff --no-ext-diff --word-diff'
+  abbr gwr 'git reset --soft'
+  abbr gwR 'git reset --hard'
+  abbr gwc 'git clean -n'
+  abbr gwC 'git clean -f'
+  abbr gwx 'git rm -r'
+  abbr gwX 'git rm -rf'
+
+  # My custom aliases
+  abbr gI 'git init'
+  abbr gaa 'git add -A'
+  abbr gis 'git status'
+  abbr gpv 'git push -v'
 end
-
-function gpp
-  git pull origin (git-branch-current)
-  git push origin (git-branch-current)
-end
-
-# Rebase (r)
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias grc='git rebase --continue'
-alias gri='git rebase --interactive'
-alias grs='git rebase --skip'
-
-# Remote (R)
-alias gR='git remote'
-alias gRl='git remote --verbose'
-alias gRa='git remote add'
-alias gRx='git remote rm'
-alias gRm='git remote rename'
-alias gRu='git remote update'
-alias gRp='git remote prune'
-alias gRs='git remote show'
-alias gRb='git-hub-browse'
-
-# Stash (s)
-alias gs='git stash'
-alias gsa='git stash apply'
-alias gsx='git stash drop'
-alias gsX='git-stash-clear-interactive'
-alias gsl='git stash list'
-alias gsL='git-stash-dropped'
-alias gsd='git stash show --patch --stat'
-alias gsp='git stash pop'
-alias gsr='git-stash-recover'
-alias gss='git stash save --include-untracked'
-alias gsS='git stash save --patch --no-keep-index'
-alias gsw='git stash save --include-untracked --keep-index'
-
-# Submodule (S)
-alias gS='git submodule'
-alias gSa='git submodule add'
-alias gSf='git submodule foreach'
-alias gSi='git submodule init'
-alias gSI='git submodule update --init --recursive'
-alias gSl='git submodule status'
-alias gSm='git-submodule-move'
-alias gSs='git submodule sync'
-alias gSu='git submodule foreach git pull origin master'
-alias gSx='git-submodule-remove'
-
-# Working Copy (w)
-alias gwd='git diff --no-ext-diff'
-alias gwD='git diff --no-ext-diff --word-diff'
-alias gwr='git reset --soft'
-alias gwR='git reset --hard'
-alias gwc='git clean -n'
-alias gwC='git clean -f'
-alias gwx='git rm -r'
-alias gwX='git rm -rf'
-
-# My custom aliases
-alias gI='git init' # init
-alias gaa='git add -A' # add all
-alias gis='git status' # status
-alias gpv='git push -v' # push verbose
