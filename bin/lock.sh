75eab38d0f37a705abdbb708d33575cbc8cb5617
Fahad Hossain <evilinfinite7@gmail.com>
2017-08-18 12:04:15 +0600

    switch to light-locker

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -blur 5x5 -scale 10% -scale 1000% /tmp/screen.png
-convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+### Lock screen on demand. Just comment out the relevant section
 
-i3lock -i /tmp/screen.png
-rm /tmp/screen.png
+# Mode 1: i3lock
+###
+# scrot /tmp/screen.png
+# convert /tmp/screen.png -blur 5x5 -scale 10% -scale 1000% /tmp/screen.png
+# convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+#
+# i3lock -i /tmp/screen.png
+# rm /tmp/screen.png
+#
+# sleep 15 && pgrep i3lock && xset dpms force off
 
-sleep 15 && pgrep i3lock && xset dpms force off
+# Mode 2: gnome-screensaver
+###
+# exec gnome-screensaver-command --lock
+
+# Mode 3: light-locker
+###
+exec light-locker-command --lock
--- bin/startapps.sh
+++ bin/startapps.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env zsh
+setxkbmap -model asus_laptop -layout us -option compose:menu
+ibus-daemon -rd
+numlockx on
+
+compton -f &
+light-locker &
+~/.fehbg
+
+xset dpms 600 600 0
+xss-lock light-locker &
+
 mpd &
+dunst &
+
 gtk-launch google-chrome
 gtk-launch clipit
 gtk-launch albert
-gnome-screensaver &
+
