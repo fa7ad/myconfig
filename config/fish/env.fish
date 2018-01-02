16358ab74a08dd94deb6208959aa0fbad0014230
Fahad Hossain <evilinfinite7@gmail.com>
2018-01-02 15:45:47 +0600

    mprisart reimplemented using feh

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -13,5 +13,5 @@ set -gx N_PREFIX /usr/local/n
 
 # Add to PATH
 # set -gx PATH $PATH /usr/local/sbin /usr/local/bin /usr/bin /usr/sbin
-set -gx PATH $PATH $HOME/.bin $HOME/.local/bin (echo $HOME/.gem/ruby/*/bin) $HOME/.cargo/bin $N_PREFIX/bin $HOME/.yarn/bin
+set -gx PATH $PATH $HOME/.bin $HOME/.local/bin (echo $HOME/.gem/ruby/*/bin) $N_PREFIX/bin $HOME/.yarn/bin
 
