95df7a31159a1063e404930d8a5e187637d1ab2e
Fahad Hossain <evilinfinite7@gmail.com>
2018-08-18 17:28:23 +0600

    add android sdk to path

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -14,14 +14,14 @@ set -gx GOPATH $HOME/go
 # N
 set -gx N_PREFIX /usr/local/n
 
-# Add to PATH
-# set -gx PATH $PATH /usr/local/sbin /usr/local/bin /usr/bin /usr/sbin
-set -gx PATH $PATH $HOME/.bin $HOME/.local/bin (string split \  $HOME/.gem/ruby/*/bin) $N_PREFIX/bin $GOPATH/bin
-
-set -U fish_user_paths $PATH
-
 # virtualfish
 set -gx VIRTUALFISH_HOME /ldata/.virtualfish
 
 # android
 set -gx ANDROID_HOME /ldata/ANDROID_SDK
+
+# Add to PATH
+# set -gx PATH $PATH /usr/local/sbin /usr/local/bin /usr/bin /usr/sbin
+set -gx PATH $PATH $HOME/.bin $HOME/.local/bin (string split \  $HOME/.gem/ruby/*/bin) $N_PREFIX/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools/bin
+
+set -U fish_user_paths $PATH
