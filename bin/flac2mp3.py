bf458e0d1f11a4f3f7571aa7bfe840ec967945a9
Fahad Hossain <evilinfinite7@gmail.com>
2018-11-10 02:43:42 +0600

    change flac2mp3

--- bin/flac2mp3.py
+++ bin/flac2mp3.py
@@ -18,33 +18,27 @@ def get_flacs(folder=None):
     if folder:
         folder = os.path.realpath(folder)
         return glob.glob("{0}/*flac".format(folder))
-    else:
-        return False
+    return False
 
 
 def convert_to_mp3(flac_file=None, output_dir=None):
     """
     Convert the specified flac to mp3 in the output_dir.
     """
-    out_dir = os.path.realpath(output_dir)
-    os.makedirs(out_dir, exist_ok=True)
-
     file_name = os.path.basename(flac_file).replace('.flac', '.mp3')
-
+    out_file = os.path.join(output_dir, file_name)
     ffmpeg = ("ffmpeg -i {0} -codec:a libmp3lame"
               " -aq 0 -ab 320k -ar 48000 -codec:v mjpeg {1}")
     ffmpeg = ffmpeg.format(
         shlex.quote(flac_file),
-        shlex.quote(out_dir + '/' + file_name))
+        shlex.quote(out_file))
 
     try:
         return subprocess.call(ffmpeg, shell=True)
     except KeyboardInterrupt:
         print("Quitting...")
-        return subprocess.call(['rm', '-fv', (out_dir + '/' + file_name)])
-    except:
-        raise
-        return 255
+        return subprocess.call(['rm', '-fv', out_file])
+    return 255
 
 
 def cli():
@@ -60,18 +54,22 @@ def cli():
     arguments = parser.parse_known_args()[0]
 
     flac_list = get_flacs(arguments.input)
+    flac_dir = os.path.basename(os.path.abspath(arguments.input))
+    out_dir = os.path.realpath(os.path.join(arguments.output, flac_dir))
+    os.makedirs(out_dir, exist_ok=True)
+
     if flac_list:
         fail = False
         for a_flac in flac_list:
             my_flac = os.path.realpath(a_flac)
-            exitcode = convert_to_mp3(my_flac, arguments.output)
-            if exitcode:
+            err = convert_to_mp3(my_flac, out_dir)
+            if err:
+                fail = True
                 break
         if fail:
             return 255
-    else:
-        print("Possibly no flac file in the input directory.")
-        return 255
+    print("Possibly no flac file in the input directory.")
+    return 255
 
 
 if __name__ == "__main__":
