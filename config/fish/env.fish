b85f3c1d69f77d381394093b4e7b8419c873391b
Fahad Hossain <evilinfinite7@gmail.com>
2019-01-05 03:47:38 +0600

    add fusuma to startup

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -23,11 +23,11 @@ set -gx ANDROID_HOME /ldata/ANDROID_SDK
 # Add to PATH
 set -l ruby_paths (string split \  $HOME/.gem/ruby/*/bin)
 set -l user_paths $HOME/.bin $HOME/.local/bin
-set -l android_paths $ANDROID_HOME/platform-tools $ANDROID_HOME/tools/bin $ANDROID_HOME/emulator 
+# set -l android_paths $ANDROID_HOME/platform-tools $ANDROID_HOME/tools/bin $ANDROID_HOME/emulator 
 set -l node_paths $N_PREFIX/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
 set -l snap_paths /snap/bin /var/lib/snapd/snap/bin
 
 
-set -gx PATH $PATH $user_paths $ruby_paths $android_paths $node_paths $GOPATH/bin $snap_path
+set -gx PATH $PATH $user_paths $ruby_paths $node_paths $GOPATH/bin $snap_path
 vardedupe PATH
 set -U fish_user_paths $PATH
