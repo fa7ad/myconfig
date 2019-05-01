47e8b43d40ae7797361014767c17ef1faf52ec87
Fahad Hossain <evilinfinite7@gmail.com>
2019-05-01 23:23:01 +0600

    use GNU stow to manage dotfiles

--- INSTALL.fish
+++ INSTALL.fish
@@ -1,23 +1,5 @@
 #!/usr/bin/env fish
 
-function _get_linkables
-  echo * |sed 's/\s/\n/g'| egrep -v '^(\.git|config|INSTALL|LICENSE|README)'
+for dir in */
+    stow $dir
 end
-
-# conf dir
-set -l confdir $HOME
-set -l files (string split \n (_get_linkables))
-
-for file in $files
-  ln -rsf $file $confdif/.$file
-done
-
-for conf in config/*
-  if [ -d "${confdir}/.${conf}" ]
-    echo backing up $conf to $conf'.bak'
-    mv $confdir/.$conf $confdir/.$conf.bak
-  fi
-
-  ln -rs ./$conf $confdir/.$conf
-done
-
---
+++ fish/.config/fish/env.fish
@@ -0,0 +1,32 @@
+#!/usr/bin/env fish
+
+bass source /etc/environment
+bass source /etc/profile
+
+# Editors
+set -gx EDITOR vim
+set -gx VISUAL vim
+set -gx PAGER less
+
+# Go
+set -gx GOPATH $HOME/go
+
+# N
+set -gx N_PREFIX /usr/local/n
+
+# virtualfish
+set -gx VIRTUALFISH_HOME /ldata/.virtualfish
+
+# android
+set -gx ANDROID_HOME /ldata/ANDROID_SDK
+
+# Add to PATH
+set -l ruby_paths (string split \  $HOME/.gem/ruby/*/bin)
+set -l user_paths $HOME/.bin $HOME/.local/bin
+set -l node_paths $N_PREFIX/bin
+set -l yarn_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin
+set -l poetry $HOME/.poetry/bin
+
+set -gx PATH $PATH $user_paths $ruby_paths $node_paths $GOPATH/bin $snap_path $yarn_paths $poetry
+vardedupe PATH
+set -U fish_user_paths $PATH
