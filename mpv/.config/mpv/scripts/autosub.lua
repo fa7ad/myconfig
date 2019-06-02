33499bad7ad54fb648a9a6c137aceec0cdddfa36
Fahad Hossain <evilinfinite7@gmail.com>
2019-06-02 20:04:42 +0600

    setwall script updated
    use oomox to generate papirus based icons

--- mpv/.config/mpv/scripts/autosub.lua
+++ mpv/.config/mpv/scripts/autosub.lua
@@ -11,7 +11,7 @@ function load_sub_fn()
     t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
     res = utils.subprocess(t)
     if res.status == 0 then
-        mp.commandv("rescan_external_files", "reselect") 
+        mp.commandv("rescan_external_files", "reselect")
         mp.msg.info("Subtitle download succeeded")
         mp.osd_message("Subtitle download succeeded")
     else
