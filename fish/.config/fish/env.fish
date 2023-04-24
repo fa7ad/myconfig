594b30659cbda032f8fc3b91d892e44452a00c1a
Fahad Hossain <set.name.fahad@gmail.com>
2023-04-24 09:06:44 +0200

    feat: install bun

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -28,6 +28,8 @@ set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbi
 set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
 set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 ### /BREW
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
 
 set -l node_paths $N_PREFIX/bin
 set -l yarn_paths $HOME/.config/yarn/global/node_modules/.bin
@@ -36,8 +38,8 @@ set -l gnu_bins (brew --prefix coreutils)/libexec/gnubin
 set -l py_paths /Users/fahad/Library/Python/*/bin
 set -l ruby_paths (brew --prefix ruby)/bin $HOMEBREW_PREFIX/lib/ruby/gems/*/bin
 set -l go_paths $HOME/go/bin
-
-set -l old_path $PATH $ruby_paths $local_paths $go_paths $node_paths $yarn_paths $py_paths # $gnu_bins
+set -l bun_path $BUN_INSTALL/bin
+set -l old_path $PATH $ruby_paths $local_paths $go_paths $node_paths $yarn_paths $py_paths $bun_path# $gnu_bins
 
 set -l new_path $old_path[1]
 set -l fish_new_path
@@ -56,4 +58,3 @@ end
 
 set -U fish_user_paths $fish_new_path
 set -gx PATH $new_path
-
