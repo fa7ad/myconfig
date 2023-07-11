3eda49865f25561bfac66ed45e42cb6d72984422
Fahad Hossain <set.name.fahad@gmail.com>
2023-07-11 21:36:03 +0200

    feat: improve brew wrapper

--- fish/.config/fish/functions/brew.fish
+++ fish/.config/fish/functions/brew.fish
@@ -1,7 +1,12 @@
 function brew -d "Wrapper function around homebrew to update brewfile automatically, made necessary since homebrew devs are dicks who prefer hacks over actually implementing features."
     command brew $argv
-    if contains install $argv; or contains remove $argv
+    if contains install $argv; or contains remove $argv; or contains upgrade $argv
         command brew bundle dump --force --global
-        sed -i '' '/^vscode.*$/d' (realpath $HOME/.Brewfile) # remove vscode
+        set -l sorted (sed -i '' '/^vscode.*$/d' (realpath $HOME/.Brewfile) | sort) #sorted for easier diffing
+        set -l temp (mktemp)
+        grep ^tap (echo $sorted | psub -f) > $temp
+        grep -v ^tap (echo $sorted | psub -f) >> $temp
+        cat $temp > (realpath $HOME/.Brewfile)
+        unlink $temp
     end
 end
