55bf1ec76d9a9d8fa65984d428b8134eed1cc436
Fahad Hossain <setname.fahad@gmail.com>
2024-11-12 23:14:41 +0100

    remove suda

--- nvim/.config/nvim/lua/config/plugins/system.lua
+++ nvim/.config/nvim/lua/config/plugins/system.lua
@@ -1,4 +1,9 @@
 return {
-  { "lambdalisue/suda.vim", lazy = true, opts = {} },
-  { "lbrayner/vim-rzip", lazy = true, opts = {} },
+  {
+    "lbrayner/vim-rzip",
+    lazy = false,
+    init = function()
+      vim.g.rzipPlugin_extra_ext = "*.odt"
+    end,
+  },
 }
